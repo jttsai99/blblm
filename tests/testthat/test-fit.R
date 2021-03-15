b96a2cae43291856b8c5cafe2809a464bb17242a
Jasper Tsai <jttsai@ucdavis.edu>
2021-03-15 14:10:25 -0700

    Added another unit test for parallel vs regular speed. Commented on unit test codes

--- tests/testthat/test-fit.R
+++ tests/testthat/test-fit.R
@@ -1,24 +1,66 @@
 test_that("Default Linear Model Estimate works", {
-  n <- 10000
+  # generate data, n has to be greater to be more accurate
+  n <- 1000
   data <- data.frame(x = rnorm(n), y = rnorm(n))
-  blblm_model = blblm(y ~ x, data)
-  lm_model = lm(y~ x, data)
-  expect_equal(coef(lm_model),coef(blblm_model),tolerance = .01)
+
+  # fit model by blblm with default parameters
+  blblm_model <- blblm(y ~ x, data)
+
+  # fit model by lm
+  lm_model <- lm(y ~ x, data)
+
+  # compare lm to its bootstrap estimate
+  expect_equal(coef(lm_model), coef(blblm_model), tolerance = .1)
 })
 
+
 test_that("Advance Linear Model Estimate works", {
+  # generate data
   n <- 1000
-  data <- data.frame(x = sample(1:1000,n,replace = TRUE), y = sample(1:1000,n,replace = TRUE))
-  blblm_model = blblm(y ~ x, data, m = 5, B = 2500, parallel = FALSE)
-  lm_model = lm(y~ x, data)
-  expect_equal(coef(lm_model),coef(blblm_model),tolerance = .01)
+  data <- data.frame(x = sample(1:1000, n, replace = TRUE), y = sample(1:1000, n, replace = TRUE))
+
+  # fit model by blblm with different parameters
+  blblm_model <- blblm(y ~ x, data, m = 5, B = 2500, parallel = FALSE)
+
+  # fit model by lm
+  lm_model <- lm(y ~ x, data)
+
+  # compare lm to its bootstrap estimate
+  expect_equal(coef(lm_model), coef(blblm_model), tolerance = .01)
 })
 
 test_that("Parallel Linear Model Estimate works", {
+  # generate data
   n <- 10000
-  data <- data.frame(x = sample(1:1000,n,replace = TRUE), y = sample(1:1000,n,replace = TRUE))
+  data <- data.frame(x = sample(1:1000, n, replace = TRUE), y = sample(1:1000, n, replace = TRUE))
+
+  # setup workers for parallel
   plan(multiprocess, workers = 4)
-  blblm_model = blblm(y ~ x, data, m = 5, B = 2500, parallel = TRUE)
-  lm_model = lm(y~ x, data)
-  expect_equal(coef(lm_model),coef(blblm_model),tolerance = .01)
+
+  # fit model by blblm with different parameters and utilize multiple cpu
+  blblm_model <- blblm(y ~ x, data, m = 5, B = 2500, parallel = TRUE)
+
+  # fit model by lm
+  lm_model <- lm(y ~ x, data)
+
+  # compare lm to its bootstrap estimate
+  expect_equal(coef(lm_model), coef(blblm_model), tolerance = .01)
+})
+
+test_that("Parallel providing faster speed works", {
+  # generate large data
+  n <- 10000
+  data <- data.frame(x = sample(1:1000, n, replace = TRUE), y = sample(1:1000, n, replace = TRUE))
+
+  # setup workers for parallel
+  plan(multiprocess, workers = 4)
+
+  # run with multiple cpu (parallel) and get elapse time
+  parallel_speed <- system.time(blblm(y ~ x, data, m = 5, B = 5000, parallel = TRUE))[3]
+
+  # run without parallel
+  regular_speed <- system.time(blblm(y ~ x, data, m = 5, B = 5000, parallel = FALSE))[3]
+
+  # parallel time is shorter than regular time
+  expect_lte(parallel_speed, regular_speed)
 })
