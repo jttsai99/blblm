3f5f193914344c34a38d0709eb3f2a973c09ddcd
Jasper Tsai <jttsai@ucdavis.edu>
2021-03-15 12:16:56 -0700

    Created a unit test for linear model estimate in test-fit.R PASS check()

--- tests/testthat/test-fit.R
+++ tests/testthat/test-fit.R
@@ -1,3 +1,7 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("Basic Linear Model Estimate works", {
+  n <- 10000
+  data <- data.frame(x = rnorm(n), y = rnorm(n))
+  blblm_model = blblm(y ~ x, data)
+  lm_model = lm(y~ x, data)
+  expect_equal(coef(lm_model),coef(blblm_model),tolerance = .01)
 })
